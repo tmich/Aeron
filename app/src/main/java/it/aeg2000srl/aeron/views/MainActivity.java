cc073edef519e3b9766c8d7b66f57231a3a71043
tmich <tiziano_michelessi@yahoo.it>
2017-09-19 13:21:35 +0200

    evidenza ordini in attesa nella Main Activity

--- app/src/main/java/it/aeg2000srl/aeron/views/MainActivity.java
+++ app/src/main/java/it/aeg2000srl/aeron/views/MainActivity.java
@@ -3,11 +3,14 @@ package it.aeg2000srl.aeron.views;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -99,4 +102,24 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // ordini in attesa
+        UseCasesService service = new UseCasesService();
+        int countOrdiniInAttesa = service.getWaitingOrders().size();
+        Button btn = (Button)btnGoToWaitingOrders;
+
+        btn.setEnabled(countOrdiniInAttesa > 0);
+        btn.setText(R.string.ordini_in_attesa);
+        //btn.setTextColor(Color.BLACK);
+
+        if(countOrdiniInAttesa > 0) {
+            //btn.setTextColor(Color.RED);
+            btn.setText(btn.getText() + " (" + countOrdiniInAttesa + ")");
+        }
+    }
+
 }
